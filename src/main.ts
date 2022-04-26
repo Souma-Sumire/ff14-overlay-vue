d56f13804eb8ad9a55613bdb1f22ea661c7c1e16
Souma <553469159@qq.com>
2022-04-26 20:00:32 +0800

    换地址

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "https://overlay.diemoe.net/common/common.min.js";
+// import "https://overlay.diemoe.net/common/common.min.js";
+import "../public/common.min.js";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
