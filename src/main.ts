c510841bbbd2a224886a86c310ddadb6846cb53e
Souma <553469159@qq.com>
2022-04-26 20:11:55 +0800

    更换url

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import "https://overlay.diemoe.net/common/common.min.js";
 import "../public/common.min.js";
 import { createPinia } from "pinia";
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
       <router-link to="/timeline/settings?OVERLAY_WS=ws://127.0.0.1:10501/ws">时间轴设置</router-link>
     </li>
     <li>
-      <a href="https://souma.diemoe.net/dist/index.html">旧导航页面</a>
+      <a href="https://soumasumire.gitee.io/ffxiv-overlay/">旧导航页面</a>
     </li>
   </ul>
 </template>
