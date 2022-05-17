7e9f39fee18b8c6d7b81a6d4f58bc5067acd8b72
Souma <553469159@qq.com>
2022-05-17 15:19:29 +0800

    可恶的element ui

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { outDir: "../Souma-Sumire.github.io/" },
+
   plugins: [
     vue(),
     AutoImport({
@@ -15,6 +17,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        // 移除打包element时的@charset警告
+        {
+          postcssPlugin: "internal:charset-removal",
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === "charset") {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
   define: {
     __SITE_IMG__: JSON.stringify(`https://cafemaker.wakingsands.com/i`),
     __SITE_IMG__BAK: JSON.stringify(`https://xivapi.com//i`),
