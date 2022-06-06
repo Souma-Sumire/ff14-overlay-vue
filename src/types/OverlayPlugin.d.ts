9dfb93b11acc70e28df04f14baf6990303e07fec
Souma <553469159@qq.com>
2022-06-07 00:25:45 +0800

    Update OverlayPlugin.d.ts

--- src/types/OverlayPlugin.d.ts
+++ src/types/OverlayPlugin.d.ts
@@ -29,3 +29,44 @@ type OverlayEvent =
   | "MiniParse"
   | "ImportedLogLines"
   | "EnmityAggroList";
+
+declare interface EnmityTargetDataEvent {
+  type: string;
+  Target: Target;
+  Focus?: any;
+  Hover?: any;
+  TargetOfTarget?: any;
+  Entries?: any;
+}
+
+interface Target {
+  ID: number;
+  OwnerID: number;
+  Type: number;
+  MonsterType: number;
+  Status: number;
+  ModelStatus: number;
+  AggressionStatus: number;
+  TargetID: number;
+  IsTargetable: boolean;
+  Job: number;
+  Name: string;
+  CurrentHP: number;
+  MaxHP: number;
+  PosX: number;
+  PosY: number;
+  PosZ: number;
+  Rotation: number;
+  Radius: number;
+  Distance: string;
+  EffectiveDistance: string;
+  Effects: Effect[];
+}
+
+interface Effect {
+  BuffID: number;
+  Stack: number;
+  Timer: number;
+  ActorID: number;
+  isOwner: boolean;
+}
