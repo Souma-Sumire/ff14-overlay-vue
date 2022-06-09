c5e4a9e56015a1d6f3c416d56043fe69f34b80ae
Souma <553469159@qq.com>
2022-06-10 00:45:19 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "https://overlay.diemoe.net/common/common.min.js";
-import "./common/common.min.js"
+import "./common/common.min.js";
 import { createPinia } from "pinia";
+// import "default-passive-events"; 
 
 const app = createApp(App);
 app.use(router);
