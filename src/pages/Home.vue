e5659669eebb4cc47176e2b44ad42943aed34e47
Souma <553469159@qq.com>
2022-08-09 05:14:50 +0800

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <ul>
-    <li>
-      <router-link to="/timeline">时间轴悬浮窗</router-link>
-      <router-link to="/timeline/settings?OVERLAY_WS=ws://127.0.0.1:10501/ws">时间轴悬浮窗设置</router-link>
-      <router-link to="/instancedAreaInfo">副本区信息显示</router-link>
-      <router-link to="/DSRP6">绝龙诗P6双目标</router-link>
-      <router-link to="/obs">OBS自动录制</router-link>
-      <router-link to="/instancedAreaInfo">副本区信息显示</router-link>
-      <!-- <router-link to="/stageProgramme">舞台节目单</router-link> -->
-    </li>
+    <li><router-link to="/timeline">时间轴悬浮窗</router-link></li>
+    <!-- <router-link to="/timeline/settings?OVERLAY_WS=ws://127.0.0.1:10501/ws">时间轴悬浮窗设置</router-link> -->
+    <li><router-link to="/instancedAreaInfo">副本区信息显示</router-link></li>
+    <li><router-link to="/DSRP6">绝龙诗P6双目标</router-link></li>
+    <li><router-link to="/obs">OBS自动录制</router-link></li>
+    <li><router-link to="/instancedAreaInfo">副本区信息显示</router-link></li>
+    <!-- <router-link to="/stageProgramme">舞台节目单</router-link> -->
     <br />
-    <li>
-      <a href="https://souma.diemoe.net/dist/">其他悬浮窗</a>
-      <a href="https://souma.diemoe.net/ffxiv_zhpatch/">国际服汉化补丁</a>
-    </li>
+    <li><a href="https://souma.diemoe.net/dist/">其他悬浮窗</a></li>
+    <li><a href="https://souma.diemoe.net/ffxiv_zhpatch/">国际服汉化补丁</a></li>
   </ul>
 </template>
 
