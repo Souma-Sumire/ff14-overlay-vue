dfe42492094644fb9b4c9c8c308e2c7d2b216abf
Souma <553469159@qq.com>
2022-08-09 04:57:04 +0800

    Update DSRP6.vue

--- src/pages/DSRP6.vue
+++ src/pages/DSRP6.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <ul v-if="actionTimeline.data.length > 0">
+    <ul v-if="actionTimeline.show">
       <li class="li-head">
         <aside>秒</aside>
         <h5>邪龙</h5>
@@ -27,7 +27,10 @@ const targetName = {
   xie: ["尼德霍格", "ニーズヘッグ", "Nidhogg"],
   sheng: ["赫拉斯瓦尔格", "フレースヴェルグ", "Hraesvelgr"],
 };
-const actionTimeline: { data: { xie: string[]; xieHP: string; sheng: string[]; shengHP: string }[] } = reactive({ data: [] });
+const actionTimeline: { show: boolean; data: { xie: string[]; xieHP: string; sheng: string[]; shengHP: string }[] } = reactive({
+  data: [],
+  show: false,
+});
 const hp: { xie: number; sheng: number } = { xie: 0, sheng: 0 };
 let baseTime = 0;
 function createACompleteImg(url: string = "000000/000405"): string {
@@ -36,6 +39,7 @@ function createACompleteImg(url: string = "000000/000405"): string {
 function handleLogLine(e: { line: string[] }): void {
   if (baseTime === 0 && e.line[0] === "20" && /^6D41$/.test(e.line[4])) {
     actionTimeline.data.length = 0;
+    actionTimeline.show = true;
     actionTimeline.data[0] = { xie: [], sheng: [], xieHP: "0", shengHP: "0" };
     baseTime = new Date().getTime();
     setTimeout(() => {
@@ -43,24 +47,25 @@ function handleLogLine(e: { line: string[] }): void {
     }, 7700);
   } else if (e.line[0] === "20" && /^(?:63C8|6D21)$/.test(e.line[4])) {
     actionTimeline.data.length = 0;
+    actionTimeline.show = false;
     actionTimeline.data[0] = { xie: [], sheng: [], xieHP: "0", shengHP: "0" };
   } else if (baseTime > 0 && (e.line[0] === "21" || e.line[0] === "22") && e.line[2][0] === "1" && e.line[6][0] === "4") {
     if (e.line[4] === "07" || e.line[4] === "08") return;
     const timeIndex = Math.round((new Date().getTime() - baseTime) / 1000);
     if (actionTimeline.data[timeIndex] === undefined)
       actionTimeline.data[timeIndex] = { xie: [], sheng: [], xieHP: hp.xie.toFixed(1), shengHP: hp.sheng.toFixed(1) };
-    console.log(1, actionTimeline.data[timeIndex].xieHP.toString(), actionTimeline.data[timeIndex].shengHP.toString());
+    // console.log(1, actionTimeline.data[timeIndex].xieHP.toString(), actionTimeline.data[timeIndex].shengHP.toString());
     const img = createACompleteImg(actionStore.getActionById(parseInt(e.line[4], 16))?.Url);
     if (targetName.xie.includes(e.line[7])) {
       hp.xie = (Number(e.line[24]) / Number(e.line[25])) * 100;
       actionTimeline.data[timeIndex].xie.push(img);
       actionTimeline.data[timeIndex].xieHP = hp.xie.toFixed(1);
-      console.log(2.1, actionTimeline.data[timeIndex].xieHP.toString());
+      // console.log(2.1, actionTimeline.data[timeIndex].xieHP.toString());
     } else if (targetName.sheng.includes(e.line[7])) {
       hp.sheng = (Number(e.line[24]) / Number(e.line[25])) * 100;
       actionTimeline.data[timeIndex].sheng.push(img);
       actionTimeline.data[timeIndex].shengHP = hp.sheng.toFixed(1);
-      console.log(2.2, actionTimeline.data[timeIndex].shengHP.toString());
+      // console.log(2.2, actionTimeline.data[timeIndex].shengHP.toString());
     }
   }
 }
