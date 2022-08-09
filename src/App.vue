09b1d2ce9a2a001b400c81a47ab21b333e7c87de
Souma <553469159@qq.com>
2022-08-10 01:51:06 +0800

    字体换源

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import UnlockedTextVue from "./components/UnlockedText.vue";
 <style lang="scss">
 @font-face {
   font-family: "SmartisanHei";
-  src: url("//ffxiv-res.diemoe.net/SmartisanHei.woff2") format("woff2"), url("//ffxiv-res.diemoe.net/SmartisanHei.ttf");
+  src: url("//cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"), url("//cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
   font-weight: normal;
   font-style: normal;
 }
