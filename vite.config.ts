e733d903fa3bb7c8daa3315e5ffee7be26aa56eb
Souma <553469159@qq.com>
2022-08-16 08:21:50 +0800

    update unocss config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Unocss from "unocss/vite";
+import { presetUno, presetAttributify, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,7 +28,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Unocss({
-      /* options */
+      presets: [presetUno(), presetAttributify(), presetIcons()],
     }),
   ],
   css: {
