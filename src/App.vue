fb9cb1412147c5374d807a687047d74109fa093d
Souma <553469159@qq.com>
2022-08-21 17:14:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
 <style lang="scss">
 @font-face {
   font-family: "SmartisanHei";
-  src: url("//cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"), url("//cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
+  src: url("//ffxiv-res.diemoe.net/SmartisanHei.woff2") format("woff2"),
+    url("//ffxiv-res.diemoe.net/SmartisanHei.ttf"),
+    url("//cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"),
+    url("//cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
   font-weight: normal;
   font-style: normal;
 }
