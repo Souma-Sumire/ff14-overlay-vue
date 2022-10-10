680e3890cbdb0032ea6755e56799957c71659dfd
Souma <553469159@qq.com>
2022-10-11 02:17:25 +0800

    Update header.vue

--- src/components/castingMonitor/header.vue
+++ src/components/castingMonitor/header.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import { useCastingMonitorStore } from "@/store/castingMonitor";
 import Util from "@/utils/util";
+import { getClassjobIconSrc } from "@/utils/xivapi";
 const castingMonitorStore = useCastingMonitorStore();
+watchEffect(() => {
+  castingMonitorStore.partyData.forEach(async (item) => {
+    item.src = await getClassjobIconSrc(item.job);
+  });
+});
 </script>
 
 <template>
-  <div>
-    <el-button-group>
-      <el-button
-        v-for="(item, index) in castingMonitorStore.partyDataFormatted"
-        :key="index"
-        plain
-        size="small"
-        @click="castingMonitorStore.changeTarget(item.id)"
-      >
-        <div flex="~ nowrap items-end" style="align-items: flex-end">
-          <img :src="castingMonitorStore.getClassjobIconSrc(item.job, 'companion')" style="height: 1.5em" />
-          {{ Util.nameToCN(Util.jobEnumToJob(item.job as number)).simple2 }}
-        </div>
-      </el-button>
-    </el-button-group>
+  <div z-100 flex="~ gap0 wrap">
+    <el-button
+      v-for="(item, index) in castingMonitorStore.partyDataFormatted"
+      :key="index"
+      size="small"
+      text
+      :type="castingMonitorStore.focusTargetId === item.id ? 'primary' : ''"
+      @click="castingMonitorStore.changeTarget(item.id)"
+      :style="castingMonitorStore.focusTargetId === item.id ? { fontWeight: 'bold' } : {}"
+      class="job-lists"
+      :class="castingMonitorStore.focusTargetId === item.id ? 'job-lists-focus' : ''"
+    >
+      <div flex="~ nowrap items-end" style="align-items: flex-end; gap: 0.1rem">
+        <img :src="item.src" style="height: 1.5em" />
+        {{ Util.nameToCN(Util.jobEnumToJob(item.job as number)).simple2 }}
+      </div>
+    </el-button>
   </div>
 </template>
