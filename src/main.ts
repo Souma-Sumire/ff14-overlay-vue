2a9795b4690042e8157e0b0764605b7a783a9abc
Souma <553469159@qq.com>
2022-10-22 06:44:19 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import App from "./App.vue";
 import router from "./router";
-// import "https://overlay.diemoe.net/common/common.min.js";
 import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 import "uno.css";
 
 const app = createApp(App);
 const head = createHead();
+const pinia = createPinia();
 app.use(router);
 app.use(head);
-app.use(createPinia());
+app.use(pinia);
 app.mount("#app");
