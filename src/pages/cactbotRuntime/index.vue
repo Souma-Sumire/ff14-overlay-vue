051d95d3d76fd8649c1b4705056cd05ce7c11a01
Souma <553469159@qq.com>
2023-01-17 08:40:01 +0800

    ✨ feat(cactbotRuntime): 优化动画效果

--- src/pages/cactbotRuntime/index.vue
+++ src/pages/cactbotRuntime/index.vue
@@ -1,83 +1,9 @@
 <script setup lang="ts">
+import "animate.css";
 const roleAssignLocationNames: Record<Role, string[]> = {
-  tank: [
-    "MT",
-    "ST",
-    "T3",
-    "T4",
-    "T5",
-    "T6",
-    "T7",
-    "T8",
-    "T9",
-    "T10",
-    "T11",
-    "T12",
-    "T13",
-    "T14",
-    "T15",
-    "T16",
-    "T17",
-    "T18",
-    "T19",
-    "T20",
-    "T21",
-    "T22",
-    "T23",
-    "T24",
-  ],
-  healer: [
-    "H1",
-    "H2",
-    "H3",
-    "H4",
-    "H5",
-    "H6",
-    "H7",
-    "H8",
-    "H9",
-    "H10",
-    "H11",
-    "H12",
-    "H13",
-    "H14",
-    "H15",
-    "H16",
-    "H17",
-    "H18",
-    "H19",
-    "H20",
-    "H21",
-    "H22",
-    "H23",
-    "H24",
-  ],
-  dps: [
-    "D1",
-    "D2",
-    "D3",
-    "D4",
-    "D5",
-    "D6",
-    "D7",
-    "D8",
-    "D9",
-    "D10",
-    "D11",
-    "D12",
-    "D13",
-    "D14",
-    "D15",
-    "D16",
-    "D17",
-    "D18",
-    "D19",
-    "D20",
-    "D21",
-    "D22",
-    "D23",
-    "D24",
-  ],
+  tank: ["MT", "ST", "T3", "T4", "T5", "T6", "T7", "T8"],
+  healer: ["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8"],
+  dps: ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8"],
   unknown: ["unknown"],
 };
 const defaultSortArray = [
@@ -103,80 +29,15 @@ const defaultSortArray = [
   "36", //青
 ];
 const fakeParty = [
-  { id: "10000001", name: "Faker1", job: 21, inParty: true },
-  { id: "10000002", name: "Faker2", job: 19, inParty: true },
-  { id: "10000003", name: "Faker3", job: 33, inParty: true },
-  { id: "10000004", name: "Faker4", job: 28, inParty: true },
-  { id: "10000005", name: "Faker5", job: 30, inParty: true },
-  { id: "10000006", name: "Faker6", job: 34, inParty: true },
-  { id: "10000007", name: "Faker7", job: 25, inParty: true },
-  { id: "10000008", name: "Faker8", job: 38, inParty: true },
+  { id: "10000001", name: "虚构战士", job: 21, inParty: true },
+  { id: "10000002", name: "虚构骑士", job: 19, inParty: true },
+  { id: "10000003", name: "虚构占星", job: 33, inParty: true },
+  { id: "10000004", name: "虚构学者", job: 28, inParty: true },
+  { id: "10000005", name: "虚构忍者", job: 30, inParty: true },
+  { id: "10000006", name: "虚构武士", job: 34, inParty: true },
+  { id: "10000007", name: "虚构黑魔", job: 25, inParty: true },
+  { id: "10000008", name: "虚构舞者", job: 38, inParty: true },
 ];
-function getJobName(job: number) {
-  switch (job.toString()) {
-    case "1":
-      return "剑术";
-    case "2":
-      return "格斗";
-    case "3":
-      return "斧术";
-    case "4":
-      return "枪术";
-    case "5":
-      return "弓箭";
-    case "6":
-      return "幻术";
-    case "7":
-      return "咒术";
-    case "19":
-      return "骑士";
-    case "20":
-      return "武僧";
-    case "21":
-      return "战士";
-    case "22":
-      return "龙骑";
-    case "23":
-      return "诗人";
-    case "24":
-      return "白魔";
-    case "25":
-      return "黑魔";
-    case "26":
-      return "秘术";
-    case "27":
-      return "召唤";
-    case "28":
-      return "学者";
-    case "29":
-      return "双剑";
-    case "30":
-      return "忍者";
-    case "31":
-      return "机工";
-    case "32":
-      return "暗骑";
-    case "33":
-      return "占星";
-    case "34":
-      return "武士";
-    case "35":
-      return "赤魔";
-    case "36":
-      return "青魔";
-    case "37":
-      return "绝枪";
-    case "38":
-      return "舞者";
-    case "39":
-      return "钐镰";
-    case "40":
-      return "贤者";
-    default: {
-      return "unknown";
-    }
-  }
-}
 const data: {
   party: {
     id: string;
@@ -197,7 +58,8 @@ function updateRoleSelectLength(): void {
   roleSelectLength.healer = data.party.reduce((p, c) => (getJobClassification(c.job) === "healer" ? p + 1 : p), 0);
   roleSelectLength.dps = data.party.reduce((p, c) => (getJobClassification(c.job) === "dps" ? p + 1 : p), 0);
 }
-if (location.href.includes("localhost")) {
+const isDev = location.href.includes("localhost");
+if (isDev) {
   handlePartyChanged({ party: fakeParty });
 }
 type Role = "tank" | "healer" | "dps" | "unknown";
@@ -208,7 +70,7 @@ function getJobClassification(job: number): Role {
   return "unknown";
 }
 function handlePartyChanged(e: { party: { id: string; name: string; inParty: boolean; job: number }[] }): void {
-  if (location.href.includes("localhost") && e.party.length === 0) return;
+  if (isDev && e.party.length === 0) return;
   data.party = e.party
     .map((p) => {
       return { ...p, rp: "" };
@@ -216,10 +78,12 @@ function handlePartyChanged(e: { party: { id: string; name: string; inParty: boo
     .sort((a, b) => defaultSortArray.indexOf(a.job.toString()) - defaultSortArray.indexOf(b.job.toString()));
   data.party.forEach((v) => (v.rp = getRP(v.job)));
   updateRoleSelectLength();
+  broadcastParty();
 }
 function handleSelectChange(i: number): void {
   const t = data.party.find((v) => v.rp === data.party[i].rp && v.id !== data.party[i].id);
   t && (t.rp = getRP(t.job));
+  broadcastParty();
 }
 function getRP(job: number): string {
   return (
@@ -227,41 +91,108 @@ function getRP(job: number): string {
     "unknown"
   );
 }
-watch(
-  data,
-  () => {
-    callOverlayHandler({
-      call: "broadcast",
-      source: "soumaRuntimeJS",
-      msg: { party: data.party },
-    });
-  },
-  { deep: true, immediate: true },
-);
+function broadcastParty(): void {
+  const sortArr = [...roleAssignLocationNames.tank, ...roleAssignLocationNames.healer, ...roleAssignLocationNames.dps];
+  data.party.sort((a, b) => sortArr.indexOf(a.rp) - sortArr.indexOf(b.rp));
+  callOverlayHandler({
+    call: "broadcast",
+    source: "soumaRuntimeJS",
+    msg: { party: data.party },
+  });
+}
+function handleChangePrimaryPlayer(event: { charID: string; charName: string }): void {
+  playerName.value = event.charName;
+}
 onMounted(() => {
   addOverlayListener("PartyChanged", handlePartyChanged);
+  addOverlayListener("ChangePrimaryPlayer", handleChangePrimaryPlayer);
   startOverlayEvents();
 });
 onBeforeUnmount(() => {
   removeOverlayListener("PartyChanged", handlePartyChanged);
+  removeOverlayListener("ChangePrimaryPlayer", handleChangePrimaryPlayer);
 });
+const mouseEnter = ref(false);
+const playerName = ref(isDev ? fakeParty[3].name : "");
+function onMouseOver(): void {
+  mouseEnter.value = true;
+}
+function onMouseOut(): void {
+  mouseEnter.value = false;
+}
 </script>
 
 <template>
-  <el-container>
-    <el-main
-      style="text-shadow: 1px 1px 2px black, -1px -1px 2px black, 1px -1px 2px black, -1px 1px 2px black; color: white">
-      <div v-for="(member, i) in data.party" :key="member.id">
-        <el-select v-model="member.rp" size="small" style="width: 3.5em" m-0 p-0 @change="handleSelectChange(i)">
-          <el-option
-            v-for="(item, index) in roleAssignLocationNames[getJobClassification(member.job)]"
-            v-show="index < roleSelectLength[getJobClassification(member.job)]"
-            :key="index"
-            :value="item"
-            :fit-input-width="true" />
-        </el-select>
-        {{ getJobName(member.job) }} {{ member.name }}
-      </div>
+  <el-container @mouseenter="onMouseOver" @mouseleave="onMouseOut">
+    <el-main>
+      <transition-group
+        name="animate__animated animate__bounce"
+        enter-active-class="animate__fadeInLeft"
+        leave-active-class="animate__fadeOutLeft">
+        <div
+          v-for="(member, i) in data.party"
+          :key="member.id"
+          v-show="mouseEnter || member.name === playerName">
+          <el-select v-model="member.rp" size="small" m-0 p-0 @change="handleSelectChange(i)" :teleported="false">
+            <el-option
+              v-for="(item, index) in roleAssignLocationNames[getJobClassification(member.job)]"
+              v-show="index < roleSelectLength[getJobClassification(member.job)]"
+              :key="index"
+              :value="item"
+              :fit-input-width="true" />
+          </el-select>
+          <span>{{ member.name }}</span>
+        </div>
+      </transition-group>
     </el-main>
   </el-container>
 </template>
+<style lang="scss" scoped>
+:deep(*) {
+  user-select: none !important;
+}
+.el-container {
+  > .el-header,
+  .el-header {
+    background-color: rgba(0, 0, 0, 0.1);
+    height: 2em;
+    position: fixed;
+    top: 0;
+  }
+  .el-main {
+    animation-duration: 0.2s;
+    padding: 0;
+    margin: 0;
+    > div {
+      animation-duration: 0.25s;
+      animation-timing-function: ease-in-out;
+      $color: rgba(0, 0, 0, 0.25);
+      > span {
+        color: white;
+        text-shadow: 1px 1px 2px $color, -1px -1px 2px $color, 1px -1px 2px $color, -1px 1px 2px $color;
+        padding-left: 0.25em;
+      }
+      :deep(*:not(.el-popper)) {
+        background-color: rgba(0, 0, 0, 0.01);
+      }
+      :deep(.el-input__inner) {
+        color: white;
+      }
+      :deep(.el-popper) {
+        background-color: rgba(255, 255, 255, 1);
+      }
+      .el-select {
+        width: 2.5em;
+        :deep(.el-input__suffix-inner) {
+          width: 0em;
+          position: relative;
+          right: 0.75em;
+        }
+        :deep(.el-input__wrapper) {
+          padding: 0px 5px;
+        }
+      }
+    }
+  }
+}
+</style>
