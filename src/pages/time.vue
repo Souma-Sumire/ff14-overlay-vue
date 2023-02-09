f016c2b375fd0aab443a035dacd53a281c63289f
Souma <553469159@qq.com>
2023-02-10 02:42:02 +0800

    feat(time):init

---
+++ src/pages/time.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import moment from "moment";
+const time = ref("");
+
+requestAnimationFrame(function update() {
+  time.value = moment().format("YYYY/MM/DD HH:mm:ss.SSS");
+  requestAnimationFrame(update);
+});
+</script>
+
+<template>
+  <span class="time">
+    {{ time }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+.time {
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  padding: 2px 5px;
+}
+</style>
