03b93f9ac4110cfcf74cc7de6b8903ed8872d5ea
Souma <553469159@qq.com>
2023-04-03 06:51:32 +0800

    style:

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,8 @@ import zhCn from "element-plus/es/locale/lang/zh-cn";
 <style lang="scss">
 @font-face {
   font-family: "SmartisanHei";
-  src: url("https://ffxiv-res.diemoe.net/SmartisanHei.woff2") format("woff2"),
-    url("https://ffxiv-res.diemoe.net/SmartisanHei.ttf"),
-    url("https://cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"),
-    url("https://cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
+  src: url("https://ffxiv-res.diemoe.net/SmartisanHei.woff2") format("woff2"), url("https://ffxiv-res.diemoe.net/SmartisanHei.ttf"),
+    url("https://cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"), url("https://cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
   font-weight: normal;
   font-style: normal;
 }
