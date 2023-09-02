029d0470361a07e6b67b303981112e9ff1af7ade
Souma-Sumire <553469159@qq.com>
2023-09-02 08:01:56 +0800

    feat(time): 显示战斗时间

--- src/pages/time.vue
+++ src/pages/time.vue
@@ -1,16 +1,43 @@
 <script setup lang="ts">
 import moment from "moment";
 const time = ref("");
+const gameIsActive = ref(false);
+const gameActiveTime = ref(0);
+const gameCombatTime = ref("");
 
 requestAnimationFrame(function update() {
   time.value = moment().format("YYYY/MM/DD HH:mm:ss.SSS");
+  if (gameActiveTime.value > 0) {
+    const currentTime = Date.now();
+    const milliseconds = currentTime - gameActiveTime.value;
+    const duration = moment.duration(milliseconds);
+    const minutes = duration.minutes();
+    const seconds = duration.seconds();
+    gameCombatTime.value = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
   requestAnimationFrame(update);
 });
+
+const handleCombatData = (e: any) => {
+  if (e.isActive === "true" && gameIsActive.value === false) {
+    gameActiveTime.value = Date.now();
+  } else if (e.isActive === "false") {
+    gameActiveTime.value = 0;
+    gameCombatTime.value = "";
+  }
+  gameIsActive.value = e.isActive === "true";
+};
+
+addOverlayListener("CombatData", handleCombatData);
+startOverlayEvents();
 </script>
 
 <template>
-  <span class="time">
+  <span class="time realTime">
     {{ time }}
+  </span>
+  <span v-if="gameCombatTime" class="time gameTime">
+    {{ gameCombatTime }}
   </span>
 </template>
 
@@ -21,5 +48,11 @@ requestAnimationFrame(function update() {
   font-size: 24px;
   font-weight: bold;
   padding: 2px 5px;
+}
+.realTime {
+  padding: 2px 5px;
+}
+.gameTime {
+  margin-left: 5px;
 }
 </style>
