503d098e00e4bdb1119f8b8c8e667a0001b9cd6e
Souma-Sumire <Souma-Sumire@users.noreply.github.com>
2023-09-28 04:58:42 +0000

    deploy: 2e395a8bb82a91adbfc694fe4c6c11a1a121cd26

---
+++ assets/queryParams-77a1fad5.js
@@ -0,0 +1,1 @@
+function s(a){if(!a)return{};let i=a.split("?")[1];if(!i)return{};let t=i.split("&");if(!t)return{};let r={};for(let l=0;l<t.length;l++){let n=t[l].split("="),e=n[0],p=n[1];r[e]?(r[e]=Array.isArray(r[e])?r[e]:[r[e]],r[e].push(p)):r[e]=p}return r}const u=s(location.href);export{u as p};
