c0df6ae501c4113095c2ff140b9263d8d7e2ea72
Souma-Sumire <553469159@qq.com>
2023-12-04 03:08:14 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,51 +1,77 @@
 <script setup lang="ts">
-import router from "@/router";
-
 type MenuType = "网页" | "悬浮窗" | "悬浮窗/网页";
 interface Menu {
   title: string;
   type: MenuType;
   path: string;
-  comment: string;
+  comment?: string;
 }
 const tableData: Menu[] = [
   {
     title: "Cactbot自定义JS分享",
     type: "网页",
     path: "https://github.com/Souma-Sumire/raidboss-user-js-public",
-    comment: "",
   },
-  // { title: "减伤监控2", type: "网页", path: "keigennRecord2", comment: "" },
-  { title: "青魔法书图鉴", type: "网页", path: "blubook", comment: "" },
-  { title: "FFLOGS上传器加速下载", type: "网页", path: "fflogsUploaderDownload", comment: "" },
-  { title: "国际服汉化补丁", type: "网页", path: "https://github.com/Souma-Sumire/FFXIVChnTextPatch-Souma/", comment: "" },
-  { title: "职业增强：舞者", type: "悬浮窗", path: "jobs/dnc", comment: "" },
-  { title: "占星一键发卡", type: "悬浮窗", path: "okASTCard", comment: "" },
+  {
+    title: "减伤监控2",
+    type: "网页",
+    path: "/#/keigennRecord2",
+  },
+  {
+    title: "青魔法书图鉴",
+    type: "网页",
+    path: "/#/blubook",
+  },
+  {
+    title: "FFLOGS上传器加速下载",
+    type: "网页",
+    path: "/#/fflogsUploaderDownload",
+  },
+  {
+    title: "国际服汉化补丁",
+    type: "网页",
+    path: "https://github.com/Souma-Sumire/FFXIVChnTextPatch-Souma/",
+  },
+  {
+    title: "职业增强：舞者",
+    type: "悬浮窗",
+    path: "/#/jobs/dnc",
+  },
+  {
+    title: "占星一键发卡",
+    type: "悬浮窗",
+    path: "/#/okASTCard",
+  },
   {
     title: "施法监控",
     type: "悬浮窗",
-    path: "castingMonitor?duration=25&energySaving=true&displayAA=false&displayGCDSpace=false&api=cafemaker&syncFocusWS=true&showHeader=true",
-    comment: "",
+    path: "/#/castingMonitor?duration=25&energySaving=true&displayAA=false&displayGCDSpace=false&api=cafemaker&syncFocusWS=true&showHeader=true",
   },
-  { title: "全副本发宏/标点", type: "悬浮窗", path: "zoneMacro", comment: "" },
-  { title: "自定义技能时间轴", type: "悬浮窗", path: "timeline", comment: "" },
-  { title: "OBS自动录制", type: "悬浮窗", path: "obs", comment: "" },
+  {
+    title: "全副本发宏/标点",
+    type: "悬浮窗",
+    path: "/#/zoneMacro",
+  },
+  {
+    title: "自定义技能时间轴",
+    type: "悬浮窗",
+    path: "/#/timeline",
+  },
+  {
+    title: "OBS自动录制",
+    type: "悬浮窗",
+    path: "/#/obs",
+  },
   {
     title: "读条汉化",
     type: "悬浮窗",
-    path: "castingToChinese",
-    comment: "",
+    path: "/#/castingToChinese",
+    comment: "国服也可以用里面的倒计时功能",
   },
   {
     title: "减伤监控（旧）",
     type: "悬浮窗",
     path: "https://souma.diemoe.net/dist/keigennRecord.html?maxLength=800&24Mode=false&bgOpacity=0.45&bodyOpacity=1&fontSize=12px&th1=36px&th2=75px&th3=34px&th4=46px&imgHeight=20&mini=false&autoclean=false",
-    comment: "预计将于7.0停止维护",
-  },
-  {
-    title: "团辅团减监控（旧）",
-    type: "悬浮窗",
-    path: "https://souma.diemoe.net/dist/keySkillTimer.html?international=false&dajinengTTS=true&jianshangTTS=true&tuanfuTTS=true",
     comment: "预计将于7.0停止维护",
   },
   {
@@ -55,21 +81,18 @@ const tableData: Menu[] = [
     comment: "预计将于7.0停止维护",
   },
   {
-    title: "LB额外增长监控（旧）",
+    title: "LB额外增长监控",
     type: "悬浮窗",
     path: "https://souma.diemoe.net/dist/limitBreakTip.html?LBMax=30000&automatic=220",
-    comment: "预计将于7.0停止维护",
+    comment: "速刷用",
   },
-  { title: "我TM现在在几线？", type: "悬浮窗", path: "instancedAreaInfo", comment: "" },
+  {
+    title: "我TM现在在几线？",
+    type: "悬浮窗",
+    path: "/#/instancedAreaInfo",
+    comment: "版本初期狩猎用",
+  },
 ];
-
-function handleClick(_e: Event, row: Menu) {
-  if (row.path.startsWith("http")) {
-    window.open(row.path, "_blank");
-  } else {
-    router.push(row.path);
-  }
-}
 </script>
 
 <template>
@@ -82,7 +105,7 @@ function handleClick(_e: Event, row: Menu) {
           <vxe-column field="type" width="100" title="类型"></vxe-column>
           <vxe-column field="path" width="100" title="链接">
             <template #default="{ row }">
-              <a @click="handleClick($event, row)">{{ row.type === "网页" ? "访问" : "跳转" }}</a>
+              <a :href="row.path" :target="row.type === '网页' ? '_blank' : '_self'">{{ row.type === "网页" ? "访问" : "跳转" }}</a>
             </template>
           </vxe-column>
           <vxe-column field="comment" title="备注"></vxe-column>
@@ -97,6 +120,5 @@ a:visited,
 a:link,
 a:hover {
   color: blue;
-  cursor: pointer;
 }
 </style>
