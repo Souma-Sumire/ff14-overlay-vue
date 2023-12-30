7e666569a69d7dc27bff143fa07cc63868d367ba
Souma-Sumire <553469159@qq.com>
2023-12-30 22:47:07 +0800

    refactor(index):

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import router from "@/router";
+import keigennRecord from "@/assets/screenshots/keigenn-record-2.png";
+import keySkillTimer from "@/assets/screenshots/key-skill-timer.png";
+import limitBreakTip from "@/assets/screenshots/limit-break-tip.gif";
+import dnc from "@/assets/screenshots/dnc.gif";
+import castingToChinese from "@/assets/screenshots/casting-to-chinese.gif";
+import teamWatch from "@/assets/screenshots/team-watch.jpg";
 
 type MenuType = "网页" | "悬浮窗" | "悬浮窗/网页";
 interface Menu {
@@ -7,17 +13,20 @@ interface Menu {
   type: MenuType;
   path: string;
   comment?: string;
+  src?: string;
 }
 const tableData: Menu[] = [
   {
     title: "Cactbot自定义JS分享",
     type: "网页",
     path: "https://github.com/Souma-Sumire/raidboss-user-js-public",
+    comment: "前往Github项目了解详情",
   },
   {
-    title: "减伤监控2",
-    type: "悬浮窗/网页",
-    path: "keigennRecord2",
+    title: "国际服汉化补丁",
+    type: "网页",
+    path: "https://github.com/Souma-Sumire/FFXIVChnTextPatch-Souma/",
+    comment: "前往Github项目了解详情",
   },
   {
     title: "青魔法书图鉴",
@@ -30,69 +39,78 @@ const tableData: Menu[] = [
     path: "fflogsUploaderDownload",
   },
   {
-    title: "国际服汉化补丁",
-    type: "网页",
-    path: "https://github.com/Souma-Sumire/FFXIVChnTextPatch-Souma/",
+    title: "[悬浮窗] 减伤监控2",
+    type: "悬浮窗/网页",
+    path: "keigennRecord2",
+    comment: "详细说明：\nhttps://nga.178.com/read.php?tid=28047277",
+    src: keigennRecord,
   },
   {
-    title: "职业增强：舞者",
+    title: "[悬浮窗] 职业增强：舞者",
     type: "悬浮窗",
     path: "jobs/dnc",
+    comment: "在技能上显示buff消失倒计时",
+    src: dnc,
   },
   {
-    title: "占星一键发卡",
+    title: "[悬浮窗] 占星一键发卡",
     type: "悬浮窗",
     path: "okASTCard",
   },
   {
-    title: "施法监控",
+    title: "[悬浮窗] 施法监控",
     type: "悬浮窗",
     path: "castingMonitor?duration=25&energySaving=true&displayAA=false&displayGCDSpace=false&api=cafemaker&syncFocusWS=true&showHeader=true",
+    comment: "详细说明：\nhttps://nga.178.com/read.php?tid=33989985",
   },
   {
-    title: "全副本发宏/标点",
+    title: "[悬浮窗] 全副本发宏/标点",
     type: "悬浮窗",
     path: "zoneMacro",
   },
   {
-    title: "自定义技能时间轴",
+    title: "[悬浮窗] 自定义技能时间轴",
     type: "悬浮窗",
     path: "timeline",
   },
   {
-    title: "OBS自动录制",
+    title: "[悬浮窗] OBS自动录制",
     type: "悬浮窗",
     path: "obs",
   },
   {
-    title: "读条汉化",
+    title: "[悬浮窗] 读条汉化",
     type: "悬浮窗",
     path: "castingToChinese",
-    comment: "国服也可以用里面的倒计时功能",
+    comment: "",
+    src: castingToChinese,
   },
   {
-    title: "减伤监控（旧）",
+    title: "[悬浮窗] 团辅监控（旧）",
     type: "悬浮窗",
-    path: "https://souma.diemoe.net/dist/keigennRecord.html?maxLength=800&24Mode=false&bgOpacity=0.45&bodyOpacity=1&fontSize=12px&th1=36px&th2=75px&th3=34px&th4=46px&imgHeight=20&mini=false&autoclean=false",
-    comment: "预计将于7.0停止维护",
+    path: "https://souma.diemoe.net/dist/keySkillTimer.html?international=false&dajinengTTS=true&jianshangTTS=true&tuanfuTTS=true",
+    comment: "",
+    src: keySkillTimer,
   },
   {
-    title: "技能监控（旧）",
+    title: "[悬浮窗] 技能监控（旧）",
     type: "悬浮窗",
     path: "https://souma.diemoe.net/dist/teamWatch.html?scale=1&reverse=false&rightAlign=false&postNamazu=false",
-    comment: "预计将于7.0停止维护",
+    comment: "建议使用dalamud同功能插件。",
+    src: teamWatch,
   },
   {
-    title: "LB额外增长监控",
+    title: "[悬浮窗] LB额外增长监控",
     type: "悬浮窗",
     path: "https://souma.diemoe.net/dist/limitBreakTip.html?LBMax=30000&automatic=220",
-    comment: "速刷用",
+    comment: "速刷用，记录LB奖励数值。",
+    src: limitBreakTip,
   },
   {
-    title: "我TM现在在几线？",
+    title: "[悬浮窗] 我TM现在在几线？",
     type: "悬浮窗",
     path: "instancedAreaInfo",
-    comment: "版本初期狩猎用",
+    comment: "狩猎用，显示你当前在几线。",
   },
 ];
 
@@ -110,15 +128,19 @@ function handleClick(e: Menu) {
     <el-container>
       <el-header><h1>主页导航</h1></el-header>
       <el-main>
-        <vxe-table :data="tableData" border>
-          <vxe-column field="title" width="250" title="名称"></vxe-column>
-          <vxe-column field="type" width="100" title="类型"></vxe-column>
-          <vxe-column field="path" width="100" title="链接">
+        <vxe-table :data="tableData" border stripe :row-config="{ height: 100 }">
+          <vxe-column width="250" title="名称">
             <template #default="{ row }">
-              <a @click="() => handleClick(row)" style="cursor: pointer">{{ row.type === "网页" ? "访问" : "跳转" }}</a>
+              <a @click="() => handleClick(row)" style="cursor: pointer">{{ row.title }}</a>
             </template>
           </vxe-column>
-          <vxe-column field="comment" title="备注"></vxe-column>
+          <vxe-column width="300" title="预览">
+            <template #default="{ row }">
+              <img v-if="row.src" :src="row.src" alt="" />
+              <i v-if="!row.src">无</i>
+            </template>
+          </vxe-column>
+          <vxe-column field="comment" type="html" title="描述" />
         </vxe-table>
       </el-main>
     </el-container>
@@ -133,5 +155,9 @@ a:hover {
 }
 a:hover {
   text-decoration: underline;
+}
+img {
+  object-fit: cover;
+  max-width: 280px;
 }
 </style>
