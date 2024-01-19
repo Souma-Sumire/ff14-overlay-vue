44d27f06adf5eec2a0277b2bdc1acd07ff01c30d
Souma-Sumire <553469159@qq.com>
2024-01-19 13:15:33 +0800

    Update hexcode.vue

--- src/pages/hexcode.vue
+++ src/pages/hexcode.vue
@@ -1,10 +1,15 @@
 <template>
   <main>
+    <h2>UTF8长度计算</h2>
+    <textarea name="" id="" cols="100" rows="5" v-model="utftext" wrap="off"></textarea>
+    <p>{{ utftextlength }}</p>
+    <h2>{{ `<hex:02开头检测` }}</h2>
     <textarea name="" id="" cols="100" rows="5" v-model="text" wrap="off"></textarea>
     <article>
+      {{ message.every((m) => m.passed) ? "全部通过" : "有错误" }}
       <ul>
         <li v-for="(m, i) in message" :key="i">
-          <span :style="{ color: m.passed ? 'black' : 'red' }">{{ `${m.msg} ${m.passed ? "通过" : `错误${m.over}`}` }}</span>
+          <span :style="{ color: m.passed ? 'black' : 'red' }">{{ `${m.passed ? "通过" : `错误`} ${m.msg} ${m.full}` }}</span>
         </li>
       </ul>
     </article>
@@ -17,45 +22,32 @@ interface Result {
   msg: string;
   passed: boolean;
   over: string;
+  full: string;
 }
+const utftext = ref("");
+const utftextlength = ref(0);
 const text = useStorage("hexcode", "");
 const message: Ref<Result[]> = ref([]);
 
-function getUnicode(s: string): number {
-  switch (true) {
-    case /[\u0000-\u007f]/.test(s):
-      return 1;
-    case /[\u0080-\u00ff]/.test(s):
-    case /[\u0100-\u017f]/.test(s):
-    case /[\u0300-\u036f]/.test(s):
-    case /[\u0370-\u03ff]/.test(s):
-    case /[\u0400-\u04ff]/.test(s):
-    case /[\u2000-\u206f]/.test(s):
-    case /[\u2070-\u209f]/.test(s):
-    case /[\u20a0-\u20cf]/.test(s):
-    case /[\u2100-\u214f]/.test(s):
-    case /[\u2150-\u218f]/.test(s):
-    case /[\u2190-\u21ff]/.test(s):
-    case /[\u2200-\u22ff]/.test(s):
-    case /[\u2300-\u23ff]/.test(s):
-    case /[\u2460-\u24ff]/.test(s):
-    case /[\u2500-\u257f]/.test(s):
-    case /[\u25a0-\u25ff]/.test(s):
-    case /[\u2600-\u26ff]/.test(s):
-    case /[\u2700-\u27bf]/.test(s):
-    case /[\u3000-\u303f]/.test(s):
-    case /[\u3040-\u309f]/.test(s):
-    case /[\u30a0-\u30ff]/.test(s):
-    case /[\u3200-\u32ff]/.test(s):
-    case /[\u3300-\u33ff]/.test(s):
-    case /[\u4e00-\u9fff]/.test(s):
-    case /[\ue000-\uf8ff]/.test(s):
-    case /[\uff00-\uffef]/.test(s):
-      return 3;
-    default:
-      console.error(`未知的${s} \\u${s.charCodeAt(0).toString(16).padStart(4, "0")}`);
-      throw new Error("Unexpected unicode range");
+watchEffect(() => {
+  utftextlength.value = getUtf8ByteLength(utftext.value);
+});
+
+function getUtf8ByteLength(str: string) {
+  let totalLength = 0;
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    if (charCode < 0x80) {
+      totalLength += 1;
+    } else if (charCode < 0x800) {
+      totalLength += 2;
+    } else if (charCode < 0x10000) {
+      totalLength += 3;
+    } else if (charCode < 0x200000) {
+      totalLength += 4;
+    }
   }
+  return totalLength;
 }
 
 function test(text: string): Result[] {
@@ -63,17 +55,12 @@ function test(text: string): Result[] {
   const result = [];
   while (lastIndex < text.length) {
     const hex02 = text.indexOf("<hex:02", lastIndex);
-    const hexFF = text.indexOf("FF", lastIndex);
-    if (hex02 === -1 && hexFF === -1) {
+    if (hex02 === -1) {
       break;
     }
-    const hex = [hex02, hexFF].filter((v) => v >= 0).sort((a, b) => a - b)[0];
-    const offset = hex === hex02 ? 0 : -7;
-    if (hex === hex02) lastIndex = hex02 + 1;
-    else if (hex === hexFF) lastIndex = hexFF + 1;
-    else throw new Error("Unexpected");
-    const code = text.slice(lastIndex + 6 + offset, lastIndex + 8 + offset);
-    const len16 = text.slice(lastIndex + 8 + offset, lastIndex + 10 + offset);
+    lastIndex = hex02 + 1;
+    const code = text.slice(lastIndex + 6, lastIndex + 8);
+    const len16 = text.slice(lastIndex + 8, lastIndex + 10);
     const len10 = parseInt(len16, 16);
     const msg = `指令${code} 长度${len16}(${len10})`;
     let i = 0;
@@ -81,8 +68,8 @@ function test(text: string): Result[] {
     let label = true;
     let colon = true;
     for (; byte < len10; i++) {
-      const b = text[hex + offset + i + 11];
-      const after = text.slice(hex + offset + i + 11, hex + offset + i + 11 + 5);
+      const b = text[hex02 + i + 11];
+      const after = text.slice(hex02 + i + 11, hex02 + i + 11 + 5);
       if (b === undefined) {
         break;
       }
@@ -94,7 +81,7 @@ function test(text: string): Result[] {
       } else if (b === ">" && colon) {
       } else if (b === "<" && after === "<hex:") {
       } else {
-        const l = getUnicode(b);
+        const l = getUtf8ByteLength(b);
         byte += label ? l / 2 : l;
         console.debug(b, "+" + (label ? l / 2 : l), `已数${byte}`);
       }
@@ -106,8 +93,9 @@ function test(text: string): Result[] {
         colon = false;
       }
     }
-    const over = text.slice(lastIndex + offset + i + 3, lastIndex + offset + i + 11);
-    result.push({ passed: over.endsWith("03>") || over.startsWith("<hex:FF"), over, msg });
+    const over = text.slice(lastIndex + i + 3, lastIndex + i + 11);
+    const full = text.slice(lastIndex - 1, lastIndex + 10 + i + 1);
+    result.push({ passed: over.endsWith("03>"), over, msg, full });
     // break;
   }
   return result;
