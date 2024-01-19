d8ac0d910a3c37077dfd0b06b13d3600cb034a7d
Souma-Sumire <553469159@qq.com>
2024-01-19 11:29:57 +0800

    Update hexcode.vue

--- src/pages/hexcode.vue
+++ src/pages/hexcode.vue
@@ -1,24 +1,6 @@
 <template>
-  <div>
-    <div><input type="file" name="" id="" @input="handleInput" /></div>
-    <div>
-      <ul>
-        <li v-for="(l, i) in fileResult" :key="i" v-show="!l.result.every((e) => e.passed)">
-          {{ l.key }}
-          {{
-            l.result.every((e) => e.passed)
-              ? "全部通过"
-              : l.result
-                  .filter((f) => f.passed === false)
-                  .map((e) => `${e.msg}${e.over}`)
-                  .join("")
-          }}
-        </li>
-      </ul>
-    </div>
-  </div>
   <main>
-    <textarea name="" id="" cols="100" rows="5" v-model="text"></textarea>
+    <textarea name="" id="" cols="100" rows="5" v-model="text" wrap="off"></textarea>
     <article>
       <ul>
         <li v-for="(m, i) in message" :key="i">
@@ -36,47 +18,60 @@ interface Result {
   passed: boolean;
   over: string;
 }
-const fileResult: Ref<{ key: string; result: Result[] }[]> = ref([]);
 const text = useStorage("hexcode", "");
 const message: Ref<Result[]> = ref([]);
-function handleInput(e: any) {
-  const file = e.target.files[0];
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    const text = e.target?.result;
-    if (text) {
-      text
-        .toString()
-        .split("\n")
-        .map((t: string, i: number) => {
-          if (i <= 4) {
-            return;
-          }
-          const [key, text] = t.split(",");
-          if (key && text) {
-            const result = test(key, text);
-            console.log(result);
-            fileResult.value.push({ key, result });
-          }
-        });
-    }
-  };
-  reader.readAsText(file);
+
+function getUnicode(s: string): number {
+  switch (true) {
+    case /[\u0000-\u007f]/.test(s):
+      return 1;
+    case /[\u0080-\u00ff]/.test(s):
+    case /[\u0100-\u017f]/.test(s):
+    case /[\u0300-\u036f]/.test(s):
+    case /[\u0370-\u03ff]/.test(s):
+    case /[\u0400-\u04ff]/.test(s):
+    case /[\u2000-\u206f]/.test(s):
+    case /[\u2070-\u209f]/.test(s):
+    case /[\u20a0-\u20cf]/.test(s):
+    case /[\u2100-\u214f]/.test(s):
+    case /[\u2150-\u218f]/.test(s):
+    case /[\u2190-\u21ff]/.test(s):
+    case /[\u2200-\u22ff]/.test(s):
+    case /[\u2300-\u23ff]/.test(s):
+    case /[\u2460-\u24ff]/.test(s):
+    case /[\u2500-\u257f]/.test(s):
+    case /[\u25a0-\u25ff]/.test(s):
+    case /[\u2600-\u26ff]/.test(s):
+    case /[\u2700-\u27bf]/.test(s):
+    case /[\u3000-\u303f]/.test(s):
+    case /[\u3040-\u309f]/.test(s):
+    case /[\u30a0-\u30ff]/.test(s):
+    case /[\u3200-\u32ff]/.test(s):
+    case /[\u3300-\u33ff]/.test(s):
+    case /[\u4e00-\u9fff]/.test(s):
+    case /[\ue000-\uf8ff]/.test(s):
+    case /[\uff00-\uffef]/.test(s):
+      return 3;
+    default:
+      console.error(`未知的${s} \\u${s.charCodeAt(0).toString(16).padStart(4, "0")}`);
+      throw new Error("Unexpected unicode range");
+  }
 }
 
-function test(key: string, text: string): Result[] {
+function test(text: string): Result[] {
   let lastIndex = 0;
   const result = [];
   while (lastIndex < text.length) {
     const hex02 = text.indexOf("<hex:02", lastIndex);
-    const hexFF = text.indexOf("<hex:FF", lastIndex);
+    const hexFF = text.indexOf("FF", lastIndex);
     if (hex02 === -1 && hexFF === -1) {
       break;
     }
-    const hex = hex02 === -1 ? hexFF : hex02;
-    const offset = hex === hexFF ? -2 : 0;
-    if (hex02 !== -1) lastIndex = hex02 + 1;
-    else if (hexFF !== -1) lastIndex = hexFF + 1;
+    const hex = [hex02, hexFF].filter((v) => v >= 0).sort((a, b) => a - b)[0];
+    const offset = hex === hex02 ? 0 : -7;
+    if (hex === hex02) lastIndex = hex02 + 1;
+    else if (hex === hexFF) lastIndex = hexFF + 1;
+    else throw new Error("Unexpected");
     const code = text.slice(lastIndex + 6 + offset, lastIndex + 8 + offset);
     const len16 = text.slice(lastIndex + 8 + offset, lastIndex + 10 + offset);
     const len10 = parseInt(len16, 16);
@@ -97,17 +92,11 @@ function test(key: string, text: string): Result[] {
       }
       if (label && !colon) {
       } else if (b === ">" && colon) {
-        continue;
       } else if (b === "<" && after === "<hex:") {
-        continue;
-      } else if (/[\u0000-\u007f]/.test(b)) {
-        byte += label ? 0.5 : 1;
-        console.debug(b, "+" + label ? 0.5 : 1, `已数${byte}`);
-      } else if (/[\ue000-\uf8ff\uff00-\uffef]/.test(b)) {
-        byte += 3;
-        console.debug(b, "+3", `已数${byte}`);
       } else {
-        console.error(`未知的${key} ${b} \\u${b.charCodeAt(0).toString(16).padStart(4, "0")}`);
+        const l = getUnicode(b);
+        byte += label ? l / 2 : l;
+        console.debug(b, "+" + (label ? l / 2 : l), `已数${byte}`);
       }
       if (b === ":") {
         colon = true;
@@ -119,14 +108,17 @@ function test(key: string, text: string): Result[] {
     }
     const over = text.slice(lastIndex + offset + i + 3, lastIndex + offset + i + 11);
     result.push({ passed: over.endsWith("03>") || over.startsWith("<hex:FF"), over, msg });
+    // break;
   }
   return result;
 }
 
 watchEffect(() => {
   message.value.length = 0;
-  const result = test("input", text.value);
-  message.value.push(...result);
+  text.value.split("\n").forEach((v) => {
+    const result = test(v);
+    message.value.push(...result);
+  });
 });
 </script>
 
