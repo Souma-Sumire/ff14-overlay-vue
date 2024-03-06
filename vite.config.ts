41efdbff4f71c4189569195a74eb9b0fee723918
Souma-Sumire <553469159@qq.com>
2024-03-06 13:40:05 +0800

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Markdown from "vite-plugin-md";
+import jsx from "@vitejs/plugin-vue-jsx";
 import { presetAttributify, presetIcons, presetUno } from "unocss";
 import Unocss from "unocss/vite";
-import viteCompression from "vite-plugin-compression";
-import Pages from "vite-plugin-pages";
 import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import jsx from "@vitejs/plugin-vue-jsx";
-import { createStyleImportPlugin, VxeTableResolve } from "vite-plugin-style-import";
+import Components from "unplugin-vue-components/vite";
 import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
+import Markdown from "vite-plugin-md";
+import Pages from "vite-plugin-pages";
+import { createStyleImportPlugin, VxeTableResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ff14-overlay-vue/",
   build: {
     outDir: "./dist",
-    emptyOutDir: true, //构建时清空outDir目录
+    emptyOutDir: true, // 构建时清空outDir目录
     rollupOptions: {
       output: {
         //   chunkFileNames: "assets/js/[name].js",
@@ -50,6 +50,11 @@ export default defineConfig({
       imports: ["vue", "@vueuse/core"],
       dts: "src/types/auto-imports.d.ts",
       resolvers: [ElementPlusResolver()],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
     }),
     Components({
       extensions: ["vue", "md"],
