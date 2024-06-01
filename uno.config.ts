3f50bc6ddf12e7f599f86d6bdc6ee1b65f3590bd
Souma-Sumire <553469159@qq.com>
2024-06-01 21:08:26 +0800

    style: unocss

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,37 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    },
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })
