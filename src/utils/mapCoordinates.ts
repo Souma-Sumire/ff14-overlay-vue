41c36f32b8d0db20fb428cdf9e6ae2657f04d477
Souma-Sumire <553469159@qq.com>
2024-07-03 05:24:15 +0800

    feat: util

---
+++ src/utils/mapCoordinates.ts
@@ -0,0 +1,45 @@
+class Vector2 {
+  x: number
+  y: number
+
+  constructor(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+  }
+
+  add(vector: Vector2): Vector2 {
+    return new Vector2(this.x + vector.x, this.y + vector.y)
+  }
+
+  divide(scalar: number): Vector2 {
+    return new Vector2(this.x / scalar, this.y / scalar)
+  }
+
+  multiply(scalar: number): Vector2 {
+    return new Vector2(this.x * scalar, this.y * scalar)
+  }
+
+  static get One(): Vector2 {
+    return new Vector2(1, 1)
+  }
+}
+
+// 将世界坐标转换为 2D 地图纹理坐标（以像素为单位）
+function getPixelCoordinates(worldXZCoordinates: Vector2, mapOffset: Vector2, mapSizeFactor: number): Vector2 {
+  const offsetVector = worldXZCoordinates.add(mapOffset)
+  const scaledVector = offsetVector.divide(100).multiply(mapSizeFactor)
+  const finalVector = scaledVector.add(new Vector2(1024, 1024))
+  return finalVector
+}
+
+// 将地图纹理像素坐标转换为游戏内 2D 地图坐标
+function getGameMapCoordinates(mapPixelCoordinates: Vector2, mapSizeFactor: number): Vector2 {
+  return mapPixelCoordinates.divide(mapSizeFactor).multiply(2).add(Vector2.One)
+}
+
+// 将世界坐标转换为游戏内 2D 地图坐标
+function worldToMapCoordinates(worldXZCoordinates: Vector2, mapOffset: Vector2, mapSizeFactor: number): Vector2 {
+  const pixelCoordinates = getPixelCoordinates(worldXZCoordinates, mapOffset, mapSizeFactor)
+  return getGameMapCoordinates(pixelCoordinates, mapSizeFactor)
+}
+export { Vector2, getPixelCoordinates, getGameMapCoordinates, worldToMapCoordinates }
