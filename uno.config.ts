4140d014340f6807b8134b0922cc61b7a191442e
Souma-Sumire <553469159@qq.com>
2024-09-16 05:18:56 +0800

    chore

--- uno.config.ts
+++ uno.config.ts
@@ -1,37 +1,4 @@
 // uno.config.ts
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetTypography,
-  presetUno,
-  presetWebFonts,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
+import { defineConfig } from 'unocss'
 
-export default defineConfig({
-  shortcuts: [
-    // ...
-  ],
-  theme: {
-    colors: {
-      // ...
-    },
-  },
-  presets: [
-    presetUno(),
-    presetAttributify(),
-    presetIcons(),
-    presetTypography(),
-    presetWebFonts({
-      fonts: {
-        // ...
-      },
-    }),
-  ],
-  transformers: [
-    transformerDirectives(),
-    transformerVariantGroup(),
-  ],
-})
+export default defineConfig({})
