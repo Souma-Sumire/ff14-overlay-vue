8ed6bd00f1ab00e7aa0bde6f1a909ae595f64f50
Souma-Sumire <553469159@qq.com>
2024-10-25 01:27:08 +0800

    feat: font link update

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,4 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </template>
 
 <style lang="scss">
-@font-face {
-  font-family: "SmartisanHei";
-  src: url("https://ffxiv-res.diemoe.net/SmartisanHei.woff2") format("woff2"), url("https://ffxiv-res.diemoe.net/SmartisanHei.ttf"),
-    url("https://cdn.diemoe.net/files/fonts/SmartisanHei.woff2") format("woff2"), url("https://cdn.diemoe.net/files/fonts/SmartisanHei.ttf");
-  font-weight: normal;
-  font-style: normal;
-}
 </style>
