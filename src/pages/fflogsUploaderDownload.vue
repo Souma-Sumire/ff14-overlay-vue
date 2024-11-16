11a25eb712b851c1b3bdda368c8b4d8dde30930b
Souma-Sumire <553469159@qq.com>
2024-11-17 00:33:58 +0800

    feat(fflogs): cdn update

--- src/pages/fflogsUploaderDownload.vue
+++ src/pages/fflogsUploaderDownload.vue
@@ -26,63 +26,32 @@ interface resJson {
 const data = reactive({ res: {} as resJson })
 const fileName = ref()
 const accelerationNodeList = [
-  [
-    'https://gh.h233.eu.org/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [@X.I.U/XIU2] 提供',
-  ],
-  [
-    'https://gh.ddlc.top/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [@mtr-static-official] 提供',
-  ],
-  [
-    'https://dl.ghpig.top/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [feizhuqwq.com] 提供',
-  ],
-  [
-    'https://slink.ltd/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [知了小站] 提供',
-  ],
-  [
-    'https://gh.con.sh/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [佚名] 提供',
-  ],
-  [
-    'https://cors.isteed.cc/github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [@Lufs\'s] 提供',
-  ],
-  [
-    'https://hub.gitmirror.com/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [GitMirror] 提供',
-  ],
-  [
-    'https://sciproxy.com/github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [sciproxy.com] 提供',
-  ],
-  [
-    'https://ghproxy.cc/https://github.com',
-    '[美国 洛杉矶] - 该公益加速源由 [@yionchiii lau] 提供',
-  ],
-  [
-    'https://cf.ghproxy.cc/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii lau] 提供',
-  ],
-  [
-    'https://gh.jiasu.in/https://github.com',
-    '[美国 Cloudflare CDN] - 该公益加速源由 [@0-RTT] 提供',
-  ],
+  ['https://gh.h233.eu.org/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@X.I.U/XIU2] 提供'],
+  ['https://gh.xiu2.us.kg/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@X.I.U/XIU2] 提供'],
+  ['https://gh.ddlc.top/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@mtr-static-official] 提供'],
+  ['https://dl.ghpig.top/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [feizhuqwq.com] 提供'],
+  ['https://slink.ltd/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [知了小站] 提供'],
+  ['https://gh.con.sh/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [佚名] 提供'],
+  ['https://gh-proxy.com/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [佚名] 提供'],
+  ['https://cors.isteed.cc/github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@Lufs\'s] 提供'],
+  ['https://hub.gitmirror.com/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [GitMirror] 提供'],
+  ['https://sciproxy.com/github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [sciproxy.com] 提供'],
+  ['https://ghproxy.cc/https://github.com', '[美国 洛杉矶] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://cf.ghproxy.cc/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://www.ghproxy.cc/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://ghproxy.cn/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://www.ghproxy.cn/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://github.site', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://github.store', '[美国 Cloudflare CDN] - 该公益加速源由 [@yionchiii] 提供'],
+  ['https://github.tmby.shop/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [blog.tmby.shop] 提供'],
+  ['https://github.moeyy.xyz/https://github.com', '[美国 Cloudflare CDN] - 该公益加速源由 [@Moeyy] 提供'],
+  ['https://hub.whtrys.space', '[美国 Cloudflare CDN] - 该公益加速源由 [FastGit 群组成员] 提供'],
   ['https://dgithub.xyz', '[美国 西雅图] - 该公益加速源由 [dgithub.xyz] 提供'],
-  [
-    'https://download.nuaa.cf',
-    '[美国 洛杉矶] - 该公益加速源由 [FastGit 群组成员] 提供',
-  ],
-  [
-    'https://download.scholar.rr.nu',
-    '[美国 纽约] - 该公益加速源由 [FastGit 群组成员] 提供',
-  ],
-  [
-    'https://download.yzuu.cf',
-    '[美国 纽约] - 该公益加速源由 [FastGit 群组成员] 提供',
-  ],
+  ['https://gh-proxy.ygxz.in/https://github.com', '[美国 洛杉矶] - 该公益加速源由 [@一个小站 www.ygxz.in] 提供'],
+  ['https://download.ixnic.net', '[美国 洛杉矶] - 该公益加速源由 [@黃埔興國] 提供'],
+  ['https://ghproxy.net/https://github.com', '[英国伦敦] - 该公益加速源由 [ghproxy]'],
+  ['https://ghp.ci/https://github.com', '[日本、韩国、新加坡、美国、德国等]（CDN 不固定） - 该公益加速源由 [ghproxy] 提供'],
+  ['https://kkgithub.com', '[中国香港、日本、韩国、新加坡等] - 该公益加速源由 [help.kkgithub.com] 提供'],
 ]
 onMounted(() => {
   fetch(
