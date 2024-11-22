71946f9a787346e7264bfaa47cc5c3141ba838d0
Souma-Sumire <553469159@qq.com>
2024-11-23 02:33:35 +0800

    chore

--- src/utils/pleaseUseACT.ts
+++
@@ -1,6 +0,0 @@
-import Swal from 'sweetalert2'
-import '@sweetalert2/theme-bootstrap-4/bootstrap-4.scss'
-
-export function pleaseUseACT(callback = () => {}) {
-  void Swal.fire('该页面需要在ACT悬浮窗中添加才可正常工作哦').finally(() => callback())
-}
