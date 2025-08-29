13f09923aa0c46af364defcb2efd43b58b74faa6
Souma-Sumire <553469159@qq.com>
2025-08-29 20:35:01 +0800

    Bypass ACT check when OVERLAY_WS param is set

--- src/composables/useActReady.ts
+++ src/composables/useActReady.ts
@@ -2,6 +2,7 @@ import { onMounted, ref } from 'vue'
 import { callOverlayHandler } from '../../cactbot/resources/overlay_plugin_api'
 import { useDev } from './useDev'
 
+const params = useUrlSearchParams('hash')
 const dev = useDev()
 
 export function useActReady() {
@@ -9,6 +10,11 @@ export function useActReady() {
 
   async function checkAct(): Promise<void> {
     if (dev.value) {
+      actReady.value = true
+      return
+    }
+    if (params.OVERLAY_WS !== undefined) {
+      // 使用WS参数时，不检测。使用场景：OBS添加需要ACT的悬浮窗，且此时未启动ACT时。
       actReady.value = true
       return
     }
