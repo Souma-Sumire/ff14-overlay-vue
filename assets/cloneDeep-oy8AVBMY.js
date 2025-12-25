fa29edd0d189ad543ee4f66040237929068fdf6a
Souma-Sumire <Souma-Sumire@users.noreply.github.com>
2025-12-25 16:51:00 +0000

    deploy: 963d7e98efd2addc19850b9bd3179aad94a245a1

--- assets/cloneDeep-oy8AVBMY.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./_baseClone-CmK6v-8I.js";function r(r){return o(r,5)}export{r as c};
---
+++ assets/index-BsD2sXkV.js
@@ -0,0 +1,1 @@
+import{A as e,u as o}from"./index-BhVp1N7m.js";const r=({from:r,replacement:s,scope:m,version:t,ref:a,type:i="API"},p)=>{e(()=>o(p),e=>{},{immediate:!0})};export{r as u};
