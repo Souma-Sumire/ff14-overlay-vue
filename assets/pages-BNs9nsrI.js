9a7839a592f73808925981532dd818f45bb69b02
Souma-Sumire <Souma-Sumire@users.noreply.github.com>
2026-06-19 05:21:28 +0000

    deploy: dbef3847811bb04bb11f462d9db0f9f7eaa2421c

--- assets/pages-BNs9nsrI.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./vendor-vue-CaWvp4-E.js";import{Yn as t,er as n,fr as r,lr as i}from"./shared-common-ChYjp3cp.js";var a=n({__name:`index`,setup(n){let a=e();return i(()=>{a.push(`/startPages`)}),(e,n)=>(r(),t(`div`))}});export{a as default};
