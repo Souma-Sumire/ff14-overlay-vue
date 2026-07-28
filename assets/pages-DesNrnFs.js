e4df6bd78be6a44506d13f24975695ef3cef1d3d
Souma-Sumire <Souma-Sumire@users.noreply.github.com>
2026-07-28 10:26:03 +0000

    deploy: 75e174a606e89764ac3dc9f95cef57b7138f913d

---
+++ assets/pages-DesNrnFs.js
@@ -0,0 +1,1 @@
+import{u as e}from"./vendor-vue-DxuZ0ZaU.js";import{Yn as t,er as n,fr as r,lr as i}from"./shared-common-CKvZkGDU.js";var a=n({__name:`index`,setup(n){let a=e();return i(()=>{a.push(`/startPages`)}),(e,n)=>(r(),t(`div`))}});export{a as default};
